8ed13ba0252bacc24e81fc946a95adf5bd9ab9d7
Tomonari Takahashi <tomo1208.japan@gmail.com>
2018-07-13 10:59:45 +0900

    fix 0203

--- ch02/ex03/popcount.go
+++ ch02/ex03/popcount.go
@@ -19,17 +19,9 @@ func PopCount(x uint64) int {
 		pc[byte(x>>(7*8))])
 }
 
-func PopCountLoop(x uint64) int {
-	var pcForLoop [256]byte
-	for i := range pcForLoop {
-		pcForLoop[i] = pcForLoop[i/2] + byte(i&1)
+func PopCountLoop(x uint64) (result int) {
+	for index := uint(0); index < 8; index++ {
+		result += int(pc[byte(x>>(index*8))])
 	}
-	return int(pcForLoop[byte(x>>(0*8))] +
-		pcForLoop[byte(x>>(1*8))] +
-		pcForLoop[byte(x>>(2*8))] +
-		pcForLoop[byte(x>>(3*8))] +
-		pcForLoop[byte(x>>(4*8))] +
-		pcForLoop[byte(x>>(5*8))] +
-		pcForLoop[byte(x>>(6*8))] +
-		pcForLoop[byte(x>>(7*8))])
+	return result
 }
