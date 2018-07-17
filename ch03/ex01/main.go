31e12758bae34457d69cf8a1a260cc3f7eb5efe1
Tomonari Takahashi <tomo1208.japan@gmail.com>
2018-07-17 20:48:03 +0900

    練習問題3.1Done

--- ch03/ex01/main.go
+++ ch03/ex01/main.go
@@ -1,21 +1,68 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
-func main() {
-	var x uint8 = 1<<1 | 1<<5
-	var y uint8 = 1<<1 | 1<<2
-	fmt.Printf("%08b\n", x)
-	fmt.Printf("%08b\n", y)
+const (
+	width   = 600
+	height  = 320
+	cells   = 100
+	xyrange = 30.0
+	xyscale = width / 2 / xyrange
+	zscale  = height * 0.4
+	angle   = math.Pi / 6
+)
 
-	for i := uint(0); i < 8; i++ {
-		if x&(1<<i) != 0 || y&(1<<i) != 0 {
-			fmt.Println(i)
-		}
+var sin30, cos30 = math.Sin(angle), math.Cos(angle)
+
+func f(x, y float64) (value float64, err bool) {
+	r := math.Hypot(x, y)
+	result := math.Sin(r) / r
+	if math.IsNaN(result) {
+		return 0, true
+	}
+	return result, false
+}
+
+func corner(i, j int) (float64, float64, bool) {
+	x := xyrange * (float64(i)/cells - 0.5)
+	y := xyrange * (float64(j)/cells - 0.5)
+	z, err := f(x, y)
+	if err {
+		return 0, 0, true
 	}
 
-	var f float64 = 3.13
-	var fMinus float64 = -3.13
-	fmt.Println(int(f))
-	fmt.Println(int(fMinus))
+	sx := width/2 + (x-y)*cos30*xyscale
+	sy := height/2 + (x+y)*sin30*xyscale - z*zscale
+	return sx, sy, false
+}
+
+func main() {
+	fmt.Printf("<svg xmls='http://www.w3.org/2000/svg' "+
+		"style='stroke: grey; fill: white; stroke-width: 0.7' "+
+		"width='%d' height='%d'>", width, height)
+	for i := 0; i < cells; i++ {
+		for j := 0; j < cells; j++ {
+			ax, ay, err := corner(i+1, j)
+			if err {
+				continue
+			}
+			bx, by, err := corner(i, j)
+			if err {
+				continue
+			}
+			cx, cy, err := corner(i, j+1)
+			if err {
+				continue
+			}
+			dx, dy, err := corner(i+1, j+1)
+			if err {
+				continue
+			}
+			fmt.Printf("<polygon points='%g,%g,%g,%g,%g,%g,%g,%g' />\n", ax, ay, bx, by, cx, cy, dx, dy)
+		}
+	}
+	fmt.Println("</svg>")
 }
